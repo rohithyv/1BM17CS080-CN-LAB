514b9dc4ec4d7e5b02b804baa33060be8dde1948
rohithyv <53818270+rohithyv@users.noreply.github.com>
2019-09-23 15:52:01 +0530

    Add files via upload

--- first.cc
+++ first.cc
@@ -35,37 +35,60 @@ main (int argc, char *argv[])
   LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
   NodeContainer nodes;
-  nodes.Create (2);
+  nodes.Create(3);
 
   PointToPointHelper pointToPoint;
   pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
   pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
   NetDeviceContainer devices;
-  devices = pointToPoint.Install (nodes);
+  devices = pointToPoint.Install (nodes.Get (0), nodes.Get (1));
 
+  NetDeviceContainer devices1;
+  devices1 = pointToPoint.Install (nodes.Get (2), nodes.Get (1));
   InternetStackHelper stack;
   stack.Install (nodes);
 
   Ipv4AddressHelper address;
   address.SetBase ("10.1.1.0", "255.255.255.0");
+Ipv4AddressHelper address1;
+  address1.SetBase ("10.1.2.0","255.255.255.0");
 
   Ipv4InterfaceContainer interfaces = address.Assign (devices);
+Ipv4InterfaceContainer interfaces1= address.Assign (devices1);
 
-  UdpEchoServerHelper echoServer (9);
+  UdpEchoServerHelper echoServer (90);
+UdpEchoServerHelper echoServer1(91); 
 
   ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
-  serverApps.Start (Seconds (1.0));
+  serverApps.Start (Seconds (1.0));     
   serverApps.Stop (Seconds (10.0));
 
-  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
+   ApplicationContainer serverApps1= echoServer1.Install (nodes.Get (1));
+  serverApps1.Start (Seconds (1.0));     
+  serverApps1.Stop (Seconds (10.0));
+
+  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 90);
   echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
   echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+  
+  UdpEchoClientHelper echoClient1 (interfaces1.GetAddress (1), 91);
+  echoClient1.SetAttribute ("MaxPackets", UintegerValue (1));
+  echoClient1.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+  echoClient1.SetAttribute ("PacketSize", UintegerValue (1024));
+  
+
 
   ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
   clientApps.Start (Seconds (2.0));
-  clientApps.Stop (Seconds (10.0));
+  clientApps.Stop (Seconds (6.0));
+
+  ApplicationContainer clientApps1= echoClient1.Install (nodes.Get (2));
+  clientApps1.Start (Seconds (7.0));
+  clientApps1.Stop (Seconds (10.0));
+
+
 
   Simulator::Run ();
   Simulator::Destroy ();
